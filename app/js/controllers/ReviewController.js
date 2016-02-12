9df6ed939f8c9a04167705cddef3e3e3f0a2d64a
Oleksandr Kobyletskyi <siadaapius@gmail.com>
2016-02-12 14:37:42 +0200

    update

--- app/js/controllers/ReviewController.js
+++ app/js/controllers/ReviewController.js
@@ -5,14 +5,10 @@ app.controller('ReviewController', ['$scope',
         //
 
         angular.forEach(true_table_data, function(value, key) {
-            angular.forEach(true_table_data, function(v, k) {
-                console.log(v);
+            angular.forEach(value, function(v, k) {
+                //console.log(v);
+                $scope.all_data.push(v);
             });
         });
-        //for(var i = 0; i < true_table_data.length; i++) {
-        //    for(var j = 0; j < true_table_data[i].length; j++) {
-        //        $scope.all_data.push(true_table_data[i][j]);
-        //    }
-        //}
     }
 ]);
